6c233319c71438f2b0873f0bbb66a7a86e53fb05
naergaga <meishuai99@163.com>
2016-09-16 12:35:48 +0800

    添加项目文件。

---
+++ Book.Naergaga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book.Naergaga.Startup))]
+namespace Book.Naergaga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
